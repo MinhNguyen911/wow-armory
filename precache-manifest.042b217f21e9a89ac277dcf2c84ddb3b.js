29eca55fb07439f7c742a0dd60744ca58ca49f69
Minh Nguyen <mhn3@sfu.ca>
2019-12-25 03:40:31 -0800

    Updates

---
+++ precache-manifest.042b217f21e9a89ac277dcf2c84ddb3b.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e4c8570eafed5f4807dde14709aef5b",
+    "url": "/wow-armory/index.html"
+  },
+  {
+    "revision": "1ae9f38e8b441b0c9efc",
+    "url": "/wow-armory/static/css/main.af3b3401.chunk.css"
+  },
+  {
+    "revision": "955d3f08f469d6d9cf76",
+    "url": "/wow-armory/static/js/2.38e91ee5.chunk.js"
+  },
+  {
+    "revision": "c37bac99a902990691b90c34ca3aaba3",
+    "url": "/wow-armory/static/js/2.38e91ee5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1ae9f38e8b441b0c9efc",
+    "url": "/wow-armory/static/js/main.7df1e061.chunk.js"
+  },
+  {
+    "revision": "a99dee1fea43413ac389",
+    "url": "/wow-armory/static/js/runtime-main.e4776dfa.js"
+  },
+  {
+    "revision": "df8be05f943a0d030627a44bb5102ade",
+    "url": "/wow-armory/static/media/background.df8be05f.jpg"
+  },
+  {
+    "revision": "927bb3d7e1213b9f3a6f209d0d0dc9de",
+    "url": "/wow-armory/static/media/raidbots.927bb3d7.jpg"
+  },
+  {
+    "revision": "5d82d619c5e5a458756fcb4fd34ee9e0",
+    "url": "/wow-armory/static/media/rio.5d82d619.png"
+  }
+]);
